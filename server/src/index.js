7bf8a9900eb713e5fec0ce234643705dcbcef0fc
Gonzalez32 <3232rose@gmail.com>
2021-09-23 10:39:40 -0500

    added more middleware logic

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
+const morgan = require('morgan'); // shows the format and helps debug.
+const helmet = require('helmet'); // block headers.
 const cors = require('cors');
 
 const app = express();
@@ -9,6 +9,29 @@ app.use(helmet());
 app.use(cors({
     origin: 'https//localhost:3000',
 }));
+
+// simple middleware message
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World',
+    });
+});
+
+// Error middleware 
+app.use((req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: error.message,
+        stack: error.stack,
+    });
+});
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
