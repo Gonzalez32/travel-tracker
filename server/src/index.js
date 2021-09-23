2a9344b68738adc1b8210117007a505fd3997e1f
Gonzalez32 <3232rose@gmail.com>
2021-09-22 22:00:26 -0500

    added morgan - index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('common'));
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:1337`);
-
+    console.log(`Listening at http://localhost:${port}`);
 });
