c72a9eb28700b9ea85c594cc19d8e458b85a3a14
Gonzalez32 <3232rose@gmail.com>
2021-09-23 17:26:03 -0500

    modified logs.js/logentry.js backend is connected

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 
+const LogEntry = require('../models/LogEntry');
+
 const router = Router();
 
 router.get('/', (req, res) => {
@@ -8,8 +10,18 @@ router.get('/', (req, res) => {
   });
 });
 
-router.post('/', (req, res) => {
-  console.log(req.body);
+router.post('/', async (req, res, next) => {
+  try {
+    const logEntry = new LogEntry(req.body);
+    const createdEntry = await logEntry.save();
+    res.json(createdEntry);
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res.status(422);
+    }
+    next(error);
+  }
+//   console.log(req.body);
 });
 
 module.exports = router;
