ff95ed7c3fbfde302dabfdf0e7f13fe41b89ffb7
Gonzalez32 <3232rose@gmail.com>
2021-09-23 10:50:55 -0500

    setup not found and error middleware

--- server/src/index.js
+++ server/src/index.js
@@ -17,19 +17,21 @@ app.get('/', (req, res) => {
     });
 });
 
-// Error middleware 
+// Error middleware
 app.use((req, res, next) => {
     const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
 });
 
+// eslint-disable-next-line no-unused-vars
+// we need four agruments: err, req, res, next
 app.use((error, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.status(statusCode);
     res.json({
         message: error.message,
-        stack: error.stack,
+        stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
     });
 });
 
