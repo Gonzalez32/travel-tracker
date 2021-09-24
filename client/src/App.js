ff9b83b0c42fe520611d6631b6dc1fb5eea90f31
Gonzalez32 <3232rose@gmail.com>
2021-09-24 12:33:59 -0500

    modified app.js/index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ReactMapGL from 'react-map-gl';
+
+import { listLogEntries } from './API';
 
 const App = () => {
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    // here should be the center of the USA map
+    latitude: 37.6,
+    longitude: -95.665,
+    zoom: 4
   });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      console.log(logEntries);
+    })();
+  }, []);
 
   return (
     <ReactMapGL
       {...viewport}
+      mapStyle="mapbox://styles/bragchump/cktymqgm601ug18pmg8n0wwo8"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
     />
