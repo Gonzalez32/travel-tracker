4b4a4b07ba9fe16271af524a732f1a059a0743bd
Gonzalez32 <3232rose@gmail.com>
2021-09-24 14:01:02 -0500

    modified App.js - changed map theme and recvied data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
 
 import { listLogEntries } from './API';
 
 const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -17,6 +18,7 @@ const App = () => {
   useEffect(() => {
     (async () => {
       const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
       console.log(logEntries);
     })();
   }, []);
@@ -27,7 +29,23 @@ const App = () => {
       mapStyle="mapbox://styles/bragchump/cktymqgm601ug18pmg8n0wwo8"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+    >
+      {
+        logEntries.map(entry => (
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude} 
+            longitude={entry.longitude} 
+            offsetLeft={-20} 
+            offsetTop={-10}
+          >
+            <div>
+              {entry.title}
+            </div>
+          </Marker>
+        ))
+      }
+    </ReactMapGL>
   );
 }
 
