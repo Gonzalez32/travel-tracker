f3e87f7b8dd16f0ba8032359f5a2646a9bcaee0f
Gonzalez32 <3232rose@gmail.com>
2021-09-29 11:53:57 -0500

    modified client - set the marker on point

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { listLogEntries } from './API';
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
   const [showPopup, setShowPopup] = useState({});
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -24,12 +25,23 @@ const App = () => {
     })();
   }, []);
 
+  // AS a user can doubleclick and point a markerpopup
+  const showAddMarkerPopup = (event) => {
+    // console.log(event);
+    const [ longitude, latitude ] = event.lngLat;
+    setAddEntryLocation({
+      latitude,
+      longitude,
+    });
+  };
+
   return (
     <ReactMapGL
       {...viewport}
       mapStyle="mapbox://styles/bragchump/cktymqgm601ug18pmg8n0wwo8"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
+      onDblClick={showAddMarkerPopup}
     >
       {
         logEntries.map(entry => (
@@ -38,23 +50,23 @@ const App = () => {
             key={entry._id}
             latitude={entry.latitude} 
             longitude={entry.longitude} 
-            onClick={() => setShowPopup({
-              ...showPopup,
-              [entry._id]: true,
-            })}
-            offsetLeft={-12} 
-            offsetTop={-24}
             >
-            {/* <div>
-              {entry.title}
-            </div> */}
-            <div>
-              <img 
-                className="Marker" 
-                src="https://i.imgur.com/y0G5YTX.png" 
-                alt="Marker"
-              />
-            </div>
+              <div
+                onClick={() => setShowPopup({
+                  // ...showPopup,
+                  [entry._id]: true,
+                })}
+              >
+                <img 
+                  className="marker"
+                  style={{
+                    height: `${6 * viewport.zoom}px`,
+                    width: `${6 * viewport.zoom}px`,
+                  }} 
+                  src="https://i.imgur.com/y0G5YTX.png" 
+                  alt="marker"
+                />
+              </div>
             </Marker>
             {
               showPopup[entry._id] ? (
@@ -63,20 +75,56 @@ const App = () => {
                   longitude={entry.longitude} 
                   closeButton={true}
                   closeOnClick={false}
+                  dynamicPosition={true}
                   onClose={() => setShowPopup({
-                    ...showPopup,
+                    // ...showPopup,
                     [entry._id]: false,
                   })}
                   anchor="top" >
                   <div className="popup">
                     <h3>{entry.title}</h3>
                     <p>{entry.comments}</p>
+                    <small>Date Visited: {new Date(entry.visitDate).toLocaleDateString()}</small>
                   </div>
                 </Popup>
               ) : null
             }
           </>
         ))
+      }
+      {
+        addEntryLocation ? (
+          <>
+          <Marker
+            latitude={addEntryLocation.latitude} 
+            longitude={addEntryLocation.longitude}
+          >
+            <div>
+              <img 
+                className="marker"
+                style={{
+                  height: `${6 * viewport.zoom}px`,
+                  width: `${6 * viewport.zoom}px`,
+                }} 
+                src="https://i.imgur.com/y0G5YTX.png" 
+                alt="marker"
+              />
+            </div>
+          </Marker>
+          <Popup
+            latitude={addEntryLocation.latitude} 
+            longitude={addEntryLocation.longitude} 
+            closeButton={true}
+            closeOnClick={false}
+            dynamicPosition={true}
+            onClose={() => setShowPopup(null)}
+            anchor="top" >
+            <div className="popup">
+              <h3> Add your new log entry here! </h3>
+            </div>
+          </Popup>
+          </>
+        ) : null
       }
     </ReactMapGL>
   );
