eac7565a8a761a5770a88fc9f6d414d59ecdb1f2
Gonzalez32 <3232rose@gmail.com>
2021-10-01 14:41:56 -0500

    new changes are modified updating v-7

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { get } from 'react-hook-form';
+// import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 
 import { listLogEntries } from './API';
@@ -31,7 +30,7 @@ const App = () => {
 
   // AS a user can doubleclick and point a markerpopup
   const showAddMarkerPopup = (event) => {
-    // console.log(event);
+    console.log(event);
     const [ longitude, latitude ] = event.lngLat;
     setAddEntryLocation({
       latitude,
@@ -44,14 +43,14 @@ const App = () => {
       {...viewport}
       mapStyle="mapbox://styles/bragchump/cktymqgm601ug18pmg8n0wwo8"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
+      onViewportChange={setViewport}
       onDblClick={showAddMarkerPopup}
     >
       {
         logEntries.map(entry => (
-          <>
+          <React.Fragment key={entry._id}>
           <Marker
-            key={entry._id}
+            // key={entry._id}
             latitude={entry.latitude} 
             longitude={entry.longitude} 
             >
@@ -86,10 +85,7 @@ const App = () => {
                   closeButton={true}
                   closeOnClick={false}
                   dynamicPosition={true}
-                  onClose={() => setShowPopup({
-                    // ...showPopup,
-                    [entry._id]: false,
-                  })}
+                  onClose={() => setShowPopup({})}
                   anchor="top" >
                   <div className="popup">
                     <h3>{entry.title}</h3>
@@ -99,7 +95,7 @@ const App = () => {
                 </Popup>
               ) : null
             }
-          </>
+          </React.Fragment>
         ))
       }
       {
@@ -111,19 +107,19 @@ const App = () => {
           >
             <div>
               <svg
-                  className="marker red"
-                  style={{
-                    height: `${6 * viewport.zoom}px`,
-                    width: `${6 * viewport.zoom}px`,
-                  }}
-                  version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
+                className="marker red"
+                style={{
+                  height: `${6 * viewport.zoom}px`,
+                  width: `${6 * viewport.zoom}px`,
+                }}
+                version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
+                <g>
                   <g>
-                    <g>
-                      <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
-                        c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
-                        c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
-                    </g>
+                    <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                      c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                      c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
                   </g>
+                </g>
               </svg>
             </div>
           </Marker>
@@ -136,7 +132,6 @@ const App = () => {
             onClose={() => setAddEntryLocation(null)}
             anchor="top" >
             <div className="popup">
-              {/* <h3> Add your new log entry here! </h3> */}
               <LogEntryForm onClose={() => {
                 setAddEntryLocation(null);
                 getEntries();
